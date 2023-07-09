e3efa590711e4c0c06272a7bf6e924fb7eb2d874
CarlMurray <carl.j.murray@gmail.com>
2023-07-09 18:10:19 +0100

    chore: add Regex validators, verb_name to models

--- core/models.py
+++ core/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+flight_number_validator = RegexValidator(regex=r'UX[0-9]+', message='Flight number must be in the format UX000')
+iata_validator = RegexValidator(regex=r'[A-Z]+', message='IATA code should be three uppercase letters')
 
 class Airport(models.Model):
-    name = models.CharField(max_length=50)
-    iata = models.CharField(max_length=3)
+    name = models.CharField(max_length=50, verbose_name='Airport name')
+    iata = models.CharField(max_length=3, validators=[iata_validator], verbose_name='IATA code')
     locality = models.CharField(max_length=50)
     region = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     
 class Flight(models.Model):
-    flight_number = models.CharField(max_length=50)
+    flight_number = models.CharField(max_length=50, validators=[flight_number_validator])
     origin = models.ForeignKey("Airport", on_delete=models.CASCADE, related_name='origin')
     destination = models.ForeignKey("Airport", on_delete=models.CASCADE, related_name='destination')
-    dep_time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    arr_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    dep_time = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name='Departure time')
+    arr_time = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name='Arrival time')
     price = models.DecimalField(max_digits=7, decimal_places=2)
     aircraft = models.ForeignKey("Aircraft", on_delete=models.CASCADE, related_name='aircraft')
     
 class Aircraft(models.Model):
     identification = models.CharField(max_length=50)
     seats = models.PositiveIntegerField()
-    ac_type = models.CharField(max_length=50)
+    ac_type = models.CharField(max_length=50, verbose_name='Aircraft type')

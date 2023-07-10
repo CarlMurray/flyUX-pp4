b17949dbd74f0f18bb7828a048c1c7cb4005112c
CarlMurray <carl.j.murray@gmail.com>
2023-07-10 22:49:23 +0100

    style: add Flatpickr date picker

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,12 @@
-let hamburgerMenu = document.querySelector('#hamburger-menu')
-let hamburgerMenuExpanded = document.querySelector('#hamburger-menu-expanded')
-let isOpen = false
-hamburgerMenu.addEventListener('click', function(){
-    isOpen = !isOpen
-    if (isOpen === true) {
-        hamburgerMenuExpanded.classList.remove('hidden')
-    }
-    else hamburgerMenuExpanded.classList.add('hidden')
-})
+let hamburgerMenu = document.querySelector("#hamburger-menu");
+let hamburgerMenuExpanded = document.querySelector("#hamburger-menu-expanded");
+let isOpen = false;
+hamburgerMenu.addEventListener("click", function () {
+  isOpen = !isOpen;
+  if (isOpen === true) {
+    hamburgerMenuExpanded.classList.remove("hidden");
+  } else hamburgerMenuExpanded.classList.add("hidden");
+});
+
+let date_picker_from = flatpickr("#flatpickr-date-outbound", {});
+let date_picker_to = flatpickr("#flatpickr-date-return", {});

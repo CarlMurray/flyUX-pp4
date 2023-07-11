ab82459cbb3c5b75bc5bcd0472f0414c92360f58
CarlMurray <carl.j.murray@gmail.com>
2023-07-11 22:45:04 +0100

    checkpoint: search results functioning

--- core/views.py
+++ core/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home_page(request):
     return render(request, 'core/index.html')
+
+def search_results_view(request):
+    trip_type = request.GET['trip_type']
+    origin = request.GET['origin']
+    destination = request.GET['destination']
+    date_outbound = request.GET['date_outbound']
+    date_return = request.GET['date_return']
+    context = {
+        'trip_type':trip_type,
+        'origin':origin,
+        'destination':destination,
+        'date_outbound':date_outbound,
+        'date_return':date_return
+    }
+    return render(request, 'core/search-results.html', context)

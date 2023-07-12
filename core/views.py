6af0c728dca31e69f249705b817019c48baa1952
CarlMurray <carl.j.murray@gmail.com>
2023-07-12 22:57:03 +0100

    feat: search results show flights

--- core/views.py
+++ core/views.py
@@ -7,18 +7,20 @@ def home_page(request):
 
 def search_results_view(request):
     trip_type = request.GET['trip_type']
-    origin = request.GET['origin']
-    destination = request.GET['destination']
+    origin = request.GET['origin'][-3:] # GETS IATA CODE
+    destination = request.GET['destination'][-3:] # GETS IATA CODE
     date_outbound = request.GET['date_outbound']
     date_return = request.GET['date_return']
-    flight_origin = Airport.objects.filter(iata=origin.upper())
+    flight_origin = Airport.objects.filter(iata=origin.upper()).get()
     print(flight_origin)
+    flight_results = Flight.objects.filter(origin=flight_origin)
     context = {
         'trip_type':trip_type,
         'origin':origin,
         'destination':destination,
         'date_outbound':date_outbound,
         'date_return':date_return,
-        'flight_origin':flight_origin
+        'flight_origin':flight_origin,
+        'flight_results':flight_results
     }
     return render(request, 'core/search-results.html', context)

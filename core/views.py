a8ed51631cd374c78a67721651ead0be8e14da11
CarlMurray <carl.j.murray@gmail.com>
2023-07-16 20:13:44 +0100

    feat: add return flight results and alt dates

--- core/views.py
+++ core/views.py
@@ -19,23 +19,33 @@ def search_results_view(request):
         destination=flight_destination,        
         outbound_date=outbound_date
         )
+    flight_results_return = Flight.objects.filter(
+        origin=flight_destination,
+        destination=flight_origin,        
+        outbound_date=return_date
+        )
     sorted_price = flight_results.values_list('price')
     lowest_price = float(sorted_price.order_by('price').first()[0])
-    slider_date_list = [] 
-    slider_date_range = range(-2, 3)
-    for x in slider_date_range:
-        date = datetime.strptime(outbound_date, '%Y-%m-%d') + timedelta(days=x)
-        slider_date_list.append(date)
-    print(slider_date_list)
     
+    def create_alt_date_range(leg_date):
+        slider_date_list = [] 
+        slider_date_range = range(-2, 3)
+        for x in slider_date_range:
+            date = datetime.strptime(leg_date, '%Y-%m-%d') + timedelta(days=x)
+            slider_date_list.append(date)
+        print(slider_date_list)
+        return slider_date_list
+    print(return_date)
     context = {
         'trip_type':trip_type,
         'origin':request.GET['origin'],
         'destination':request.GET['destination'],
         'outbound_date':datetime.strptime(outbound_date, '%Y-%m-%d'),
-        'return_date':return_date,
+        'return_date':datetime.strptime(return_date, '%Y-%m-%d'),
         'lowest_price':lowest_price,
         'flight_results':flight_results,
-        'slider_date_list':slider_date_list
+        'slider_date_list':create_alt_date_range(outbound_date),
+        'slider_date_list_return':create_alt_date_range(return_date),
+        'flight_results_return':flight_results_return,
     }
     return render(request, 'core/search-results.html', context)
--- static/js/alt-date-select.js
+++ static/js/alt-date-select.js
@@ -1,13 +1,28 @@
 let altDates = document.querySelectorAll('.altDate')
-let dateInput = document.querySelector('#alt-date-input')
+let altDatesReturn = document.querySelectorAll('.altDateReturn')
+let dateInputOutbound = document.querySelector('#alt-date-input-outbound')
+let dateInputReturn = document.querySelector('#alt-date-input-return')
 let form = document.querySelector('#alt-date-select-form')
-
+console.log(dateInputOutbound)
+console.log(dateInputReturn)
 // ADD CLICK LISTENER TO EACH ALT DATE
 altDates.forEach(function(item){
     item.addEventListener('click', function(){
     selectedDate = this.id
     // SET INPUT VALUE TO SELECTED DATE
-    dateInput.setAttribute('value', selectedDate)
+    dateInputOutbound.setAttribute('value', selectedDate)
     // SUBMIT FORM TO LOAD NEW FLIGHTS LIST
     form.submit()
 })})
+
+// ADD CLICK LISTENER TO EACH ALT DATE
+altDatesReturn.forEach(function(item){
+    item.addEventListener('click', function(){
+    selectedDate = this.id
+    // SET INPUT VALUE TO SELECTED DATE
+    dateInputReturn.setAttribute('value', selectedDate)
+    // SUBMIT FORM TO LOAD NEW FLIGHTS LIST
+    form.submit()
+})})
+
+// TODO: REFACTOR SCRIPT

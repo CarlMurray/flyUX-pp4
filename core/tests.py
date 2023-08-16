4a7a178691adb117fe059a4f8aeb65b4949a0c01
CarlMurray <carl.j.murray@gmail.com>
2023-08-16 20:25:39 +0100

    chore: build home_page view tests; code formatting

--- core/tests.py
+++ core/tests.py
@@ -1,17 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import *
 from datetime import datetime
+from .views import *
 
 
 class AiportTestCase(TestCase):
     def setUp(self):
-        Airport.objects.create(name='Testport', iata='TST',
-                               locality='Test', region='Tester', country='Testing')
+        Airport.objects.create(
+            name="Testport",
+            iata="TST",
+            locality="Test",
+            region="Tester",
+            country="Testing",
+        )
 
     def test_airport_str(self):
-        a = Airport.objects.get(iata='TST')
+        a = Airport.objects.get(iata="TST")
         print(a)
-        self.assertEqual(str(a), 'Testport (TST)')
+        self.assertEqual(str(a), "Testport (TST)")
 
 
 class FlightTestCase(TestCase):
@@ -19,70 +25,67 @@ class FlightTestCase(TestCase):
         Airport.objects.bulk_create(
             [
                 Airport(
-                    name='Origin',
-                    iata='ORG',
-                    locality='Org',
-                    region='Ogn',
-                    country='Orgtest'
+                    name="Origin",
+                    iata="ORG",
+                    locality="Org",
+                    region="Ogn",
+                    country="Orgtest",
                 ),
                 Airport(
-                    name='Destination',
-                    iata='DST',
-                    locality='Dest',
-                    region='Dst',
-                    country='Dsttest'
+                    name="Destination",
+                    iata="DST",
+                    locality="Dest",
+                    region="Dst",
+                    country="Dsttest",
                 ),
             ]
         )
         Aircraft.objects.create(
-            identification='TEST123',
-            seats=100,
-            aircraft_type='b737-700'
+            identification="TEST123", seats=100, aircraft_type="b737-700"
         )
-        origin = Airport.objects.get(iata='ORG')
-        destination = Airport.objects.get(iata='DST')
+        origin = Airport.objects.get(iata="ORG")
+        destination = Airport.objects.get(iata="DST")
         aircraft = Aircraft.objects.first()
         Flight.objects.create(
-            flight_number='UX00001',
+            flight_number="UX00001",
             origin=origin,
             destination=destination,
-            outbound_date=datetime.strptime('2023-01-05', '%Y-%m-%d'),
+            outbound_date=datetime.strptime("2023-01-05", "%Y-%m-%d"),
             dep_time=datetime.now(),
             arr_time=datetime.now(),
             price=100,
-            aircraft=aircraft
+            aircraft=aircraft,
         )
 
     def test_flight_str(self):
-        flight = Flight.objects.get(flight_number='UX00001')
-        self.assertEqual(
-            str(flight), 'UX00001, Origin (ORG) --> Destination (DST)')
+        flight = Flight.objects.get(flight_number="UX00001")
+        self.assertEqual(str(flight), "UX00001, Origin (ORG) --> Destination (DST)")
 
     def test_standard_fare(self):
-        flight = Flight.objects.get(flight_number='UX00001')
+        flight = Flight.objects.get(flight_number="UX00001")
         self.assertEqual(flight.standard_fare(), flight.price)
 
     def test_plus_fare(self):
-        flight = Flight.objects.get(flight_number='UX00001')
-        self.assertEqual(flight.plus_fare(), flight.price+17)
+        flight = Flight.objects.get(flight_number="UX00001")
+        self.assertEqual(flight.plus_fare(), flight.price + 17)
 
     def test_premium_fare(self):
-        flight = Flight.objects.get(flight_number='UX00001')
-        self.assertEqual(flight.premium_fare(), flight.price+31)
+        flight = Flight.objects.get(flight_number="UX00001")
+        self.assertEqual(flight.premium_fare(), flight.price + 31)
 
     def test_get_fare(self):
-        flight = Flight.objects.get(flight_number='UX00001')
-        self.assertEqual(flight.get_fare('Standard'), flight.price)
-        self.assertEqual(flight.get_fare('Plus'), flight.price+17)
-        self.assertEqual(flight.get_fare('Premium'), flight.price+31)
+        flight = Flight.objects.get(flight_number="UX00001")
+        self.assertEqual(flight.get_fare("Standard"), flight.price)
+        self.assertEqual(flight.get_fare("Plus"), flight.price + 17)
+        self.assertEqual(flight.get_fare("Premium"), flight.price + 31)
 
 
 class AircraftTestCase(TestCase):
     def setUp(self):
         Aircraft.objects.create(
-            identification='TEST123',
+            identification="TEST123",
             seats=100,
-            aircraft_type=Aircraft.AIRCRAFT_TYPES[0][1]
+            aircraft_type=Aircraft.AIRCRAFT_TYPES[0][1],
         )
 
     def test_aircraft_str(self):
@@ -92,8 +95,8 @@ class AircraftTestCase(TestCase):
 
 class PassengerTestCase(TestCase):
     def test_passenger_str(self):
-        passenger = Passenger(first='Test', last='McTesterson')
-        self.assertEqual(str(passenger), 'Test McTesterson')
+        passenger = Passenger(first="Test", last="McTesterson")
+        self.assertEqual(str(passenger), "Test McTesterson")
 
 
 class BookingTestCase(TestCase):
@@ -101,59 +104,120 @@ class BookingTestCase(TestCase):
         Airport.objects.bulk_create(
             [
                 Airport(
-                    name='Origin',
-                    iata='ORG',
-                    locality='Org',
-                    region='Ogn',
-                    country='Orgtest'
+                    name="Origin",
+                    iata="ORG",
+                    locality="Org",
+                    region="Ogn",
+                    country="Orgtest",
                 ),
                 Airport(
-                    name='Destination',
-                    iata='DST',
-                    locality='Dest',
-                    region='Dst',
-                    country='Dsttest'
+                    name="Destination",
+                    iata="DST",
+                    locality="Dest",
+                    region="Dst",
+                    country="Dsttest",
                 ),
             ]
         )
         Aircraft.objects.create(
-            identification='TEST123',
+            identification="TEST123",
             seats=100,
-            aircraft_type=Aircraft.AIRCRAFT_TYPES[0][1]
+            aircraft_type=Aircraft.AIRCRAFT_TYPES[0][1],
         )
         Flight.objects.create(
-            flight_number='UX00001',
-            origin=Airport.objects.get(iata='ORG'),
-            destination=Airport.objects.get(iata='DST'),
-            outbound_date=datetime.strptime('2023-01-05', '%Y-%m-%d'),
+            flight_number="UX00001",
+            origin=Airport.objects.get(iata="ORG"),
+            destination=Airport.objects.get(iata="DST"),
+            outbound_date=datetime.strptime("2023-01-05", "%Y-%m-%d"),
             dep_time=datetime.now(),
             arr_time=datetime.now(),
             price=100,
-            aircraft=Aircraft.objects.first()
+            aircraft=Aircraft.objects.first(),
         )
         Flight.objects.create(
-            flight_number='UX00002',
-            origin=Airport.objects.get(iata='DST'),
-            destination=Airport.objects.get(iata='ORG'),
-            outbound_date=datetime.strptime('2023-01-05', '%Y-%m-%d'),
+            flight_number="UX00002",
+            origin=Airport.objects.get(iata="DST"),
+            destination=Airport.objects.get(iata="ORG"),
+            outbound_date=datetime.strptime("2023-01-05", "%Y-%m-%d"),
             dep_time=datetime.now(),
             arr_time=datetime.now(),
             price=100,
-            aircraft=Aircraft.objects.first()
+            aircraft=Aircraft.objects.first(),
         )
         Booking.objects.create(
-            reference='52abb40b-7c2d-4d2d-a7b0-589f7687abd1',
-            outbound_flight=Flight.objects.get(flight_number='UX00001'),
-            return_flight=Flight.objects.get(flight_number='UX00002'),
-            customer=User.objects.create(email='test@text.com',
-                                         first_name='Test', last_name='McTesterson'),
+            reference="52abb40b-7c2d-4d2d-a7b0-589f7687abd1",
+            outbound_flight=Flight.objects.get(flight_number="UX00001"),
+            return_flight=Flight.objects.get(flight_number="UX00002"),
+            customer=User.objects.create(
+                email="test@text.com", first_name="Test", last_name="McTesterson"
+            ),
         )
 
     def test_booking_str(self):
         booking = Booking.objects.first()
-        self.assertEqual(str(
-            booking), 'Reference: 52ABB40B; Origin (ORG) - Destination (DST); Test McTesterson')
+        self.assertEqual(
+            str(booking),
+            "Reference: 52ABB40B; Origin (ORG) - Destination (DST); Test McTesterson",
+        )
 
     def test_get_shorthand(self):
         booking = Booking.objects.first()
-        self.assertEqual(booking.get_shorthand(), '52ABB40B')
+        self.assertEqual(booking.get_shorthand(), "52ABB40B")
+
+
+# -------------------------------------
+# VIEWS TESTS
+# -------------------------------------
+
+
+class HomePageViewTestCase(TestCase):
+    """
+    Test case for testing home_page view.
+    """
+    def setUp(self):
+        """
+        Creates Airports for tests.
+        Defines Client().
+        """
+        Airport.objects.bulk_create(
+            [
+                Airport(
+                    name="Origin",
+                    iata="ORG",
+                    locality="Org",
+                    region="Ogn",
+                    country="Orgtest",
+                ),
+                Airport(
+                    name="Destination",
+                    iata="DST",
+                    locality="Dest",
+                    region="Dst",
+                    country="Dsttest",
+                ),
+            ]
+        )
+
+        self.client = Client()
+
+    def test_home_page_templates(self):
+        """
+        Tests templates used for home_page view.
+        Simulates a request to home_page. 
+        Adds each template name to template_list. 
+        Compares template_list with template name strings.
+        """
+        request = self.client.get("")
+        template_list = []
+        for t in request.templates:
+            template_list.append(t.name)
+        self.assertListEqual(template_list, ["core/index.html", "base/base.html"])
+
+    def test_home_page_context(self):
+        """
+        Tests context passed from home_page view to template.
+        Asserts that airports Queryset same as context.
+        """
+        request = self.client.get("")
+        airports = Airport.objects.all()
+        self.assertQuerysetEqual(request.context["airports"], airports, ordered=False)

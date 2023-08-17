87701ef15bc0b99e1e8a258a2147810e180ae22e
CarlMurray <carl.j.murray@gmail.com>
2023-08-17 19:50:57 +0100

    chore: add custom error templates

--- flyux/settings.py
+++ flyux/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv("DEBUG") == "TRUE")
 
 # '*' TO ALLOW FOR LOCAL MOBILE TESTING
 ALLOWED_HOSTS = ['*','https://flyux-b4bb85443186.herokuapp.com/', '*.herokuapp.com', 'https://flyux.carlmurray.design/']

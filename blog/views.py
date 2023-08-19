460c27ecc450c3c0a2ac3329f0ef853b5abf6ef1
CarlMurray <carl.j.murray@gmail.com>
2023-08-19 11:23:44 +0100

    build: add related posts to blog detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import BlogPost
+import random
+
 
 def blog_view(request):
     posts = BlogPost.objects.all()
@@ -8,9 +10,12 @@ def blog_view(request):
     }
     return render(request, "blog/blog.html", context)
 
+
 def blog_detail(request, id):
     post = BlogPost.objects.get(id=id)
+    more_posts = random.sample(list(BlogPost.objects.exclude(id=id)), 3)
     context = {
-        'post': post
+        'post': post,
+        'more_posts': more_posts,
     }
     return render(request, "blog/blog-detail.html", context)

c2002461a9a10367575ead0989089aeaa61820ca
CarlMurray <carl.j.murray@gmail.com>
2023-08-19 12:35:24 +0100

    style: add transition to hamburger menu

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,19 @@
 let hamburgerMenu = document.querySelector("#hamburger-menu");
 let hamburgerMenuExpanded = document.querySelector("#hamburger-menu-expanded");
+let hamburgerNavLinks = document.querySelector("#hamburger-nav-links");
 let isOpen = false;
 hamburgerMenu.addEventListener("click", function () {
   isOpen = !isOpen;
   if (isOpen === true) {
-    hamburgerMenuExpanded.classList.remove("hidden");
-  } else hamburgerMenuExpanded.classList.add("hidden");
+    // hamburgerMenuExpanded.classList.remove("max-h-0");
+    hamburgerMenuExpanded.classList.toggle("max-h-screen");
+    hamburgerMenuExpanded.classList.toggle("p-12");
+    // hamburgerNavLinks.classList.toggle("hidden");
+  } else {
+        hamburgerMenuExpanded.classList.toggle("max-h-screen");
+        hamburgerMenuExpanded.classList.toggle("p-12");
+    // hamburgerNavLinks.classList.toggle("hidden");
+  }
 });
 
 // let date_picker_from = flatpickr("#flatpickr-date-outbound", {});
